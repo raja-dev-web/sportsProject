b7cb65f8fb935e7ef99a8d2f04e9386eb77cbccb
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-08 15:15:46 +0100

    Rendered HTML template in flask

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -6,4 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to our Website"
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
