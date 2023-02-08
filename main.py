fcb7bc68e61b506563bcf0cf3608c7a4bc14aab6
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-08 20:00:49 +0100

    new module added

--- main.py
+++ main.py
@@ -7,7 +7,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    score_data = [
+        {
+            "id": 1,
+            "starting_time": "16:00",
+            "team_a": "Random Team 1",
+            "score": "0 - 0",
+            "team_b": "Random Team 2",
+            "minute": "00:00",
+        },
+        {
+            "id": 2,
+            "starting_time": "18:00",
+            "team_a": "Random Team 3",
+            "score": "0 - 0",
+            "team_b": "Random Team 4",
+            "minute": "00:00",
+        },
+    ]
+    return render_template(
+        'index.html',
+        games=score_data
+    )
 
 
 if __name__ == "__main__":
