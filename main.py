e16af955c139b0f402cb966491ce3baa48f7bf87
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2023-02-08 21:16:21 +0100

    script added

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/json_data')
+def json_data():
     score_data = [
         {
             "id": 1,
             "starting_time": "16:00",
             "team_a": "Random Team 1",
-            "score": "0 - 0",
+            "score": "1 - 2",
             "team_b": "Random Team 2",
-            "minute": "00:00",
+            "minute": "10:00",
         },
         {
             "id": 2,
             "starting_time": "18:00",
             "team_a": "Random Team 3",
-            "score": "0 - 0",
+            "score": "1 - 3",
             "team_b": "Random Team 4",
-            "minute": "00:00",
+            "minute": "20:00",
         },
     ]
+    return jsonify(score_data)
+
+
+@app.route('/')
+def index():
+    score_data = json_data()
     return render_template(
         'index.html',
-        games=score_data
+        games=score_data.json
     )
 
 
---
+++ static/scripts/update_score.js
@@ -0,0 +1,35 @@
+//console.log("javascript working!")
+setInterval(function() {
+    fetch('/json_data').then(
+        response => response.json()
+        ).then(
+            data =>
+                data.forEach(game =>
+                    updateElement(game)
+                )
+        )
+    },20000
+);
+
+function updateElement(game){
+    Object.entries(game).forEach(([k,v]) => {
+        element = document.getElementById(k + "_" + game.id);
+        // Get Previous value first
+        previousValue = element.innerHTML;
+
+        // If Previous value is not equal to the new value, change it!
+        if (previousValue !== v.toString()) {
+            blinkUpdate(element, v.toString())
+        }
+    })
+}
+
+function blinkUpdate(element, newValue, newColor='#00ff00') {
+    previousColor = element.style.color;
+    element.innerHTML = newValue;
+    element.style.color = newColor;
+    // Set back to original color after timeout ms
+    setTimeout(() => {
+        element.style.color = previousColor
+    }, 3000)
+}
